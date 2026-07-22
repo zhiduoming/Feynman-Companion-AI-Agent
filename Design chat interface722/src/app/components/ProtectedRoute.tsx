693e050122a5a993fc1c3a8511f61c9dd94fb4ca
lllemon-tree <3965481732@qq.com>
2026-07-22 21:19:42 +0800

    feat: 登录注册与会话持久化

--- Design chat interface722/src/app/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router";
-import { isLoggedIn } from "../lib/auth";
-
-export function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  if (!isLoggedIn()) {
-    return <Navigate to="/auth" replace />;
-  }
-  return <>{children}</>;
-}
